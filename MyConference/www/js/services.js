5c30ed808dd7139eff69d3a0d08ffbc1608b1bbe
Clemens Hübner <clemens.huebner@gmail.com>
2016-05-24 17:14:50 +0200

    Added license to javascript files

--- MyConference/www/js/services.js
+++ MyConference/www/js/services.js
@@ -1,5 +1,18 @@
-/**
- * Created by Murad Isayev on 08.05.2016.
+/*
+ This file is part of MyConference.
+
+ MyConference is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License version 3
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should find a copy of the GNU Affero General Public License in the
+ root directory along with this program.
+ If not, see http://www.gnu.org/licenses/agpl-3.0.html.
  */
 
 var services = angular.module('services', []);
@@ -63,7 +76,7 @@ services.factory('backendService', function () {
         console.log("Update error ", error);
       })
   }
-   
+
   backend.createEvent = function (ev) {
     var newEvent = new Object();
     newEvent.title = ev.title;
