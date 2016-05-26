524c113f8b403e143841f3cf6731a2c21d6adc91
Clemens Hübner <clemens.huebner@gmail.com>
2016-05-26 14:45:36 +0200

    Added docker commands to deploy script

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,4 +3,7 @@ set -e
 # Script for deploy image to docker hub
 
 cd MyConference/
-
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker pull osrgroup/amos-downloader-base-image:1.0
+docker build -t clemenshuebner/amos-ss16-proj8:1.0 docker/
+docker push clemenshuebner/amos-ss16-proj8:1.0
