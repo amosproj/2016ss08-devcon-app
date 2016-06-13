ee03a360e7da99c0c1f03becd45558051b4a12c8
soniahidri <hidri.sonia@gmail.com>
2016-06-13 14:32:39 +0200

    services.js : added a function for saving object.

--- MyConference/www/js/services.js
+++ MyConference/www/js/services.js
@@ -112,6 +112,14 @@ services.factory('backendService', function ($rootScope, $q, $filter) {
     backend.resetPassword = function (user) {
       BaasBox.resetPasswordForUser(user);
     };
+    
+     /*
+     Function for saving object
+     returns a promise
+     */
+    backend.save = function (object, collection) {
+      return BaasBox.save(object, collection);
+    };
     /*
      Function for updating user account
      requires 2 parameters: field to update and object with data that should be updated. See Baasbox API documentation
