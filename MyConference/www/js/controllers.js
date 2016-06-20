c5072cf28e7f3e183215100dac38228f6f1227ca
Murad Isayev <murad.isayev.93@mail.ru>
2016-06-20 15:54:16 +0200

    controlles.js: added new controller for updating user settings

--- MyConference/www/js/controllers.js
+++ MyConference/www/js/controllers.js
@@ -1403,4 +1403,25 @@ angular.module('starter.controllers', ['services', 'ngCordova'])
           $scope.afterSubmit = true;
         })
     }
+  })
+
+  .controller('SettingsCtrl', function ($scope, backendService, $state, $ionicPopup, $translate) {
+    userInfo = backendService.currentUser.visibleByTheUser;
+    if(typeof userInfo.settings === 'undefined') userInfo.settings = {"pushNotification": "yes"};
+    $scope.yesSelected = userInfo.settings.pushNotification === "yes" ? true : false;
+    $scope.updateSettings = function (settings) {
+      userInfo.settings = settings;
+      backendService.updateUserProfile({"visibleByTheUser": userInfo}).then(function (res) {
+        $translate("Done!").then(
+          function (de) {
+            $ionicPopup.alert({
+              title: de,
+              template: "{{'Settings are updated' | translate}}"
+            }).then(function (re) {
+              $state.go('app.main');
+            });
+          }
+        );
+      })
+    }
   });
