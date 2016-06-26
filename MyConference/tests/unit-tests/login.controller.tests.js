6a63c5747007f2d4d21b502e48499f6c3b9e6da2
trungluongquang <trungluongquang@gmail.com>
2016-06-26 20:59:32 +0200

    1 ok 1 fail

--- MyConference/tests/unit-tests/login.controller.tests.js
+++ MyConference/tests/unit-tests/login.controller.tests.js
@@ -1,7 +1,13 @@
-/*
+
 describe('example test', function() {
   it('should be true', function() {
     expect('foo1').toBe('foo');
   });
 });
-*/
+
+
+describe('example test', function() {
+  it('should be true', function() {
+    expect('foo').toBe('foo');
+  });
+});
