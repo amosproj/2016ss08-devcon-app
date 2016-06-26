5a7b939cf48c3b7219663daba951d02312f1302e
trungluongquang <trungluongquang@gmail.com>
2016-06-26 21:00:38 +0200

    bower install

--- MyConference/tests/unit-tests/login.controller.tests.js
+++ MyConference/tests/unit-tests/login.controller.tests.js
@@ -1,11 +1,3 @@
-
-describe('example test', function() {
-  it('should be true', function() {
-    expect('foo1').toBe('foo');
-  });
-});
-
-
 describe('example test', function() {
   it('should be true', function() {
     expect('foo').toBe('foo');
