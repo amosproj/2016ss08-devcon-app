473cd7a53ef7864af8647cd4720dfa02c77ab347
trungluongquang <trungluongquang@gmail.com>
2016-06-26 21:13:13 +0200

    failed test

--- MyConference/tests/unit-tests/login.controller.tests.js
+++ MyConference/tests/unit-tests/login.controller.tests.js
@@ -3,3 +3,16 @@ describe('example test', function() {
     expect('foo').toBe('foo');
   });
 });
+
+describe('example test', function() {
+  it('should be true', function() {
+    expect('foo').toBe('foo1');
+  });
+});
+
+
+describe('example test', function() {
+  it('should be true', function() {
+    expect('foo1').toBe('foo');
+  });
+});
