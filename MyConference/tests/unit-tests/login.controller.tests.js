86847971572cd8bc73cb22321b2da59c1395a05e
Murad Isayev <murad.isayev.93@mail.ru>
2016-06-28 15:26:35 +0200

    added module and controller loading, as well as created mocks for login testing, wrote 1 test case

--- MyConference/tests/unit-tests/login.controller.tests.js
+++ MyConference/tests/unit-tests/login.controller.tests.js
@@ -1,5 +1,62 @@
-describe('example test', function() {
-  it('should be true', function() {
-    expect('foo').toBe('foo');
-  });
-});
+describe('LoginCtrl:', function() {
+  var scope,
+    backendServiceMock,
+    q,
+    ctrl,
+    credentials,
+    ionicPopupMock,
+    translateMock,
+    dfd;
+
+  beforeEach(module('starter'));
+
+  beforeEach(module(function($provide, $urlRouterProvider) {
+    $provide.value('$ionicTemplateCache', function(){} );
+    $urlRouterProvider.deferIntercept();
+  }));
+
+  beforeEach(inject(function ($rootScope, $controller, $q) {
+    credentials = {
+      username: 'blabla',
+      password: 'blabla'
+    }
+    dfd = $q.defer();
+    backendServiceMock = {
+      login: function (username, password) {
+        return dfd.promise;
+      },
+      logout: function () {
+        return dfd.promise;
+      }
+    }
+    ionicPopupMock = jasmine.createSpyObj('$ionicPopup spy', ['alert']);
+    translateMock = function () {
+      return dfd.promise()
+    }
+    spyOn(backendServiceMock, 'login').and.callThrough();
+    scope = $rootScope.$new();
+    ctrl = $controller('LoginCtrl', {
+      $scope: scope,
+      $ionicPopup: ionicPopupMock,
+      $translate: translateMock,
+      backendService: backendServiceMock
+    });
+    scope.login(credentials);
+  }));
+  
+  // tests
+  describe('login function', function () {
+    it('should call backendService.login function', function () {
+
+      //expect backendService.login function to be called with the credentials that was inputted by user
+      expect(backendServiceMock.login).toHaveBeenCalledWith('blabla', 'blabla');
+    })
+
+    it('if executed successfully, should call alert about successful login', function () {
+      //simulate successful login
+
+    })
+
+  })
+
+})
