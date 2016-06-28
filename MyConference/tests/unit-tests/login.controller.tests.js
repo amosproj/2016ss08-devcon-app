1954bcd49235415cf08d4cf4082c6723c5f25103
Murad Isayev <murad.isayev.93@mail.ru>
2016-06-28 19:49:18 +0200

    login.controller.tests.js: added new test cases for login function

--- MyConference/tests/unit-tests/login.controller.tests.js
+++ MyConference/tests/unit-tests/login.controller.tests.js
@@ -14,43 +14,43 @@
  root directory along with this program.
  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
  */
-
-describe('LoginCtrl:', function() {
+describe('LoginCtrl:', function () {
   var scope,
     backendServiceMock,
-    q,
     ctrl,
     credentials,
     ionicPopupMock,
     translateMock,
-    dfd;
-
+    httpBackend,
+    loginDfd,
+    eventDfd,
+    translateDfd,
+    alertDfd;
   beforeEach(module('starter'));
-
-  beforeEach(module(function($provide, $urlRouterProvider) {
-    $provide.value('$ionicTemplateCache', function(){} );
+  beforeEach(module(function ($provide, $urlRouterProvider) {
+    $provide.value('$ionicTemplateCache', function () {
+    });
     $urlRouterProvider.deferIntercept();
   }));
-
-  beforeEach(inject(function ($rootScope, $controller, $q) {
+  beforeEach(inject(function ($rootScope, $controller, $q, $httpBackend) {
+    $httpBackend.whenGET('locales/de.json').respond(200, '');
     credentials = {
       username: 'blabla',
       password: 'blabla'
     }
-    dfd = $q.defer();
+    loginDfd = $q.defer();
+    eventDfd = $q.defer();
+    translateDfd = $q.defer();
+    alertDfd = $q.defer();
     backendServiceMock = {
-      login: function (username, password) {
-        return dfd.promise;
-      },
-      logout: function () {
-        return dfd.promise;
-      }
+      login: jasmine.createSpy('login spy').and.returnValue(loginDfd.promise),
+      logout: jasmine.createSpy('logout spy').and.returnValue(loginDfd.promise),
+      applySettingsForCurrentUser: jasmine.createSpy('logout spy'),
+      getEvents: jasmine.createSpy('getEvents spy').and.returnValue(eventDfd.promise)
     }
-    ionicPopupMock = jasmine.createSpyObj('$ionicPopup spy', ['alert']);
-    translateMock = function () {
-      return dfd.promise()
-    }
-    spyOn(backendServiceMock, 'login').and.callThrough();
+    ionicPopupMock = jasmine.createSpyObj('$ionicPopup spy', ['alert'])
+    translateMock = jasmine.createSpy('$translate spy').and.returnValue(translateDfd.promise)
+    ionicPopupMock.alert.and.returnValue(alertDfd.promise)
     scope = $rootScope.$new();
     ctrl = $controller('LoginCtrl', {
       $scope: scope,
@@ -60,20 +60,53 @@ describe('LoginCtrl:', function() {
     });
     scope.login(credentials);
   }));
-
   // tests
   describe('login function', function () {
-    it('should call backendService.login function', function () {
-
-      //expect backendService.login function to be called with the credentials that was inputted by user
+    it('should call backendService.login function with the credentials that was inputted by user', function () {
       expect(backendServiceMock.login).toHaveBeenCalledWith('blabla', 'blabla');
     })
-
-    it('if executed successfully, should call alert about successful login', function () {
-      //simulate successful login
-
+    describe('if executed successfully, ', function () {
+      beforeEach(function () {
+        //simulate successful login
+        loginDfd.resolve([]);
+        scope.$digest();
+      })
+      it('should call backenService.getEvents function', function () {
+        expect(backendServiceMock.getEvents).toHaveBeenCalled();
+      })
+      it('should call $translate service with "Done!" message', function () {
+        eventDfd.resolve([]);
+        scope.$digest();
+        expect(translateMock).toHaveBeenCalledWith('Done!');
+      })
+      it('should call alert about successful login', function () {
+        eventDfd.resolve([]);
+        scope.$digest();
+        translateDfd.resolve('Done!');
+        scope.$digest();
+        expect(ionicPopupMock.alert).toHaveBeenCalledWith({
+          title: 'Done!',
+          template: "{{'Login successful.' | translate}}"
+        })
+      })
     })
-
+    describe('if executed unsuccessfully, ', function () {
+      beforeEach(function () {
+        //simulate unsuccessful login
+        loginDfd.reject([]);
+        scope.$digest();
+      })
+      it('should call $translate service with "Error!" message', function () {
+        expect(translateMock).toHaveBeenCalledWith('Error!');
+      })
+      it('should call alert about unsuccessful login', function () {
+        translateDfd.resolve('Error!');
+        scope.$digest();
+        expect(ionicPopupMock.alert).toHaveBeenCalledWith({
+          title: 'Error!',
+          template: "{{'Username and password did not match.' | translate}}"
+        })
+      })
+    })
   })
-
 })
