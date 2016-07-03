85f2c90e5ac2c5adbfa1d07cef3b5f85e7fca836
Clemens Hübner <clemens.huebner@gmail.com>
2016-07-03 20:57:32 +0200

    Changed create event tests

--- MyConference/tests/unit-tests/new_event.tests.js
+++ MyConference/tests/unit-tests/new_event.tests.js
@@ -65,7 +65,7 @@ describe('LoginCtrl: ', function () {
       $translate: translateMock,
       backendService: backendServiceMock
     });
-    scope.createEvent(credentials);
+    scope.createEvent(mockEvent);
   }));
   // tests
   describe('createEvent function ', function () {
