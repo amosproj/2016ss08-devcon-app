88491896eae88a6091ae69e744eefc1e7e0ea502
Clemens Hübner <clemens.huebner@gmail.com>
2016-07-07 13:43:59 +0200

    Change previousEvents function

--- MyConference/www/js/controllers.js
+++ MyConference/www/js/controllers.js
@@ -314,6 +314,9 @@ angular.module('starter.controllers', ['services', 'ngCordova'])
      This method is used for filter after prevoius events in the main view
      */
     $scope.previousEvents = function (item) {
+      if(item.date == undefined){
+        return true;
+      }
       var itemDate = new Date(item.date);
       return today < itemDate;
     };
