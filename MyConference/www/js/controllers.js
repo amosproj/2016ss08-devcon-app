13a3d57a5483b1e90948f07a318bde3aa3944974
soniahidri <hidri.sonia@gmail.com>
2016-07-10 22:14:21 +0200

    controllers.js : updated areOrganizerAllowedToEdit () to be availible to the organizer until 48 hours after finishing an event.

--- MyConference/www/js/controllers.js
+++ MyConference/www/js/controllers.js
@@ -907,7 +907,7 @@ angular.module('starter.controllers', ['services', 'ngCordova'])
       lastEndTime = borderTimes.lastEndTime;
 
       eventDateSplitted = splitEventDateIntoPartsWithCorrectingTimezone($scope.event.date);
-      endDate = new Date(eventDateSplitted[2], eventDateSplitted[1] - 1, eventDateSplitted[0], lastEndTime.getHours() , lastEndTime.getMinutes(), 0, 0);
+      endDate = new Date(eventDateSplitted[2], eventDateSplitted[1] - 1, eventDateSplitted[0], lastEndTime.getHours() + 48, lastEndTime.getMinutes(), 0, 0);
       now = new Date();
       if (now <= endDate ) {
 
